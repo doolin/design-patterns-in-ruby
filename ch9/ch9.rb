dfee42e7c08d96075f1f663fa300d57712389dd2
Dave Doolin <david.doolin@gmail.com>
2016-06-21 18:30:23 -0700

    ch 9, p.164, encrypt method

--- ch9/ch9.rb
+++ ch9/ch9.rb
@@ -1,8 +1,31 @@
 #!/usr/bin/env ruby
 
 class Encrypter
+  def initialize(key)
+    @key = key
+  end
+
+  def encrypt(reader, writer)
+    key_index = 0
+    while not reader.eof?
+      clear_char = reader.getc
+      encrypted_char = clear_char ^ @key[key_index]
+      writer.putc(encrypted_char)
+      key_index = (key_index + 1) % @key.size
+    end
+  end
 end
 
 describe Encrypter do
-  it 'encrypts'
+  before :each do
+    @key = [1, 2, 3]
+  end
+
+  it 'instantiates' do
+    expect(Encrypter.new(@key)).to_not be nil
+  end
+
+  it 'encrypts' do
+  encrypter = Encrypter.new(@key)
+  end
 end
