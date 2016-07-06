8e2e6562a635746d132040be6c9b5af01344081c
David Doolin <david.doolin@gmail.com>
2016-07-06 03:53:41 -0700

    p. 209 incrementing class and instance counters

--- ch12/ch12.rb
+++ ch12/ch12.rb
@@ -1,6 +1,8 @@
 require 'rspec'
 
 class ClassVariableTester
+  attr_reader :instance_count
+
   @@class_count = 0
 
   def initialize
@@ -12,13 +14,26 @@ class ClassVariableTester
     @instance_count += 1
   end
 
+  def class_count
+    @@class_count
+  end
+
   def to_s
     "@@class_count: #{@@class_count}, @instance_count: #{@instance_count}"
   end
 end
 
+
 describe ClassVariableTester do
   it 'instantiates' do
     expect(ClassVariableTester.new).not_to be nil
+  end
+
+  it 'increments class and instance counts' do
+    cl = ClassVariableTester.new
+    cl.increment
+    cl.increment
+    expect(cl.class_count).to be 2
+    expect(cl.instance_count).to be 2
   end
 end
