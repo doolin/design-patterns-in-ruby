be168cb15f80de5b1fa3e7f1fb35b389f44a3a15
David Doolin <david.doolin@gmail.com>
2016-07-09 03:40:53 -0700

    a note on class vs instance self

--- ch12/ch12.rb
+++ ch12/ch12.rb
@@ -50,3 +50,27 @@ describe 'class methods and instance methods are different' do
     expect { SomeClass.a_method }.to raise_error(NoMethodError)
   end
 end
+
+## A note for myself on self following up on p. 209
+describe 'self vs. self' do
+  class Foo
+    def self.self_id
+      # self from class
+      self.object_id
+    end
+
+    def instance_id
+      # self from instance
+      self.object_id
+    end
+  end
+
+  it 'does not have the same id for the class as the object' do
+    class_id = Foo.object_id
+    instance = Foo.new
+    instance_id = instance.object_id
+    expect(Foo.self_id).to eq class_id
+    expect(instance.instance_id).to eq instance_id
+    expect(instance_id).not_to eq class_id
+  end
+end
