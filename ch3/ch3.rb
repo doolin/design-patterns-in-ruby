3c84ea8364707f09eb0c821f345699f83496eeb2
David Doolin <david.doolin@gmail.com>
2017-09-05 04:22:33 -0700

    reengineering template example

--- ch3/ch3.rb
+++ ch3/ch3.rb
@@ -7,20 +7,34 @@ require 'rspec/autorun'
 class Report
   def initialize
     @title = 'monthly report'
-    @test = [ 'Things are going', 'really, really well']
+    @text = [ 'Things are going', 'really, really well']
   end
 
   # I would have named this simply 'output', or possibly
   # 'write'.
   def output_report
-    puts('<html>')
-    puts('  <head>')
-    puts("    <title>#{title}</title>")
-    puts('  </head>')
-    puts('  <body>')
-    @text.each { |line| puts("      <p>#{line}</p>") }
-    puts('  </body>')
-    puts('</html>')
+    report = ""
+    report << '<html>'
+    report << '  <head>'
+    report << "    <title>#{@title}</title>"
+    report << '  </head>'
+    report << '  <body>'
+    @text.each { |line| report << "      <p>#{line}</p>" }
+    report << '  </body>'
+    report << '</html>'
+  end
+
+  def output_report
+    output_start
+    output_head
+    output_body_start
+    output_body
+    output_body_end
+    output_end
+  end
+
+  def output_start
+    raise "You must override #{__method__} in subclass"
   end
 end
 
@@ -28,6 +42,20 @@ RSpec.describe Report do
   describe '.new' do
     it 'instantiates' do
       expect(Report.new).not_to be nil
+    end
+  end
+
+  describe '#output_report' do
+    xit'' do
+      expect(described_class.new.output_report).to match(/<title>monthly report<\/title>/)
+    end
+  end
+
+  describe 'output_start' do
+    it 'expects override' do
+      expect {
+        described_class.new.output_start
+      }.to raise_error("You must override output_start in subclass")
     end
   end
 end
