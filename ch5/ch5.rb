5853bac90ae44c5b83ca28f3d92102d8e68c372c
David Doolin <david.doolin@gmail.com>
2017-10-09 06:35:49 -0700

    p. 98 more observer pattern

--- ch5/ch5.rb
+++ ch5/ch5.rb
@@ -8,17 +8,30 @@ class Employee
   attr_reader :name
   attr_accessor :title, :salary
 
-  def initialize(name, title, salary)
+  def initialize(name, title, salary, payroll)
     @name = name
     @title = title
     @salary = salary
+    @payroll = payroll
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    @payroll.update(self)
+  end
+end
+
+class Payroll
+  def update(changed_employee)
+    puts "name: #{changed_employee.name}"
+    puts "salary: #{changed_employee.salary}"
   end
 end
 
 RSpec.describe Employee do
   describe 'new' do
     it 'instantiates' do
-      expect(Employee.new('foo', 'bar', 'baz')).not_to be nil
+      expect(Employee.new('foo', 'bar', 'baz', Payroll.new)).not_to be nil
     end
   end
 end
