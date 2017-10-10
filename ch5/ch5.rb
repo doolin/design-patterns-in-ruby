3699e737a83f54b056a0454ef746728d3ddae146
David Doolin <david.doolin@gmail.com>
2017-10-10 04:18:20 -0700

    p. 99 add observer

--- ch5/ch5.rb
+++ ch5/ch5.rb
@@ -8,16 +8,30 @@ class Employee
   attr_reader :name
   attr_accessor :title, :salary
 
-  def initialize(name, title, salary, payroll)
+  def initialize(name, title, salary)
     @name = name
     @title = title
     @salary = salary
-    @payroll = payroll
+    @observers = []
   end
 
   def salary=(new_salary)
     @salary = new_salary
-    @payroll.update(self)
+    notify_observers
+  end
+
+  def notify_observers
+    @observers.each do |observer|
+      observer.update(self)
+    end
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def delete_observer(observer)
+    @observers.delete(observer)
   end
 end
 
@@ -31,7 +45,7 @@ end
 RSpec.describe Employee do
   describe 'new' do
     it 'instantiates' do
-      expect(Employee.new('foo', 'bar', 'baz', Payroll.new)).not_to be nil
+      expect(Employee.new('foo', 'bar', 'baz')).not_to be nil
     end
   end
 end
