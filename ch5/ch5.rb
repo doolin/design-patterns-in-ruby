32a910f57d5215674fc8479ff7422ab49aac1f99
David Doolin <david.doolin@gmail.com>
2017-10-11 03:50:31 -0700

    p. 99 spec added for observer pattern

--- ch5/ch5.rb
+++ ch5/ch5.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'rspec/autorun'
+require 'pry'
 
 # Page 96: Observer pattern.
 
@@ -46,6 +47,23 @@ RSpec.describe Employee do
   describe 'new' do
     it 'instantiates' do
       expect(Employee.new('foo', 'bar', 'baz')).not_to be nil
+    end
+  end
+
+  context 'give Fred a raise' do
+    let(:fred) { Employee.new('Fred', 'Crane Operator', 30_000.0) }
+
+    describe '#salary' do
+      example 'fred starts with 30k' do
+        expect(fred.salary).to eq 30_000.0
+      end
+
+      example 'fred gets a raise' do
+        payroll = Payroll.new
+        fred.add_observer(payroll)
+        expect(payroll).to receive(:update).with(fred)
+        fred.salary = 35_000.0
+      end
     end
   end
 end
