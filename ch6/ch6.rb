6a7257170ffe085032f7af438c4fd0c569d31abb
David Doolin <david.doolin@gmail.com>
2017-10-20 08:05:00 -0700

    p. 115 inching along

--- ch6/ch6.rb
+++ ch6/ch6.rb
@@ -14,6 +14,26 @@ class Task
   end
 end
 
+class AddDryIngredientsTask < Task
+  def initialize
+    super('add dry ingredients')
+  end
+
+  def get_time_required
+    1.0
+  end
+end
+
+class MixTask < Task
+  def initialize
+    super('mix ingredients')
+  end
+
+  def get_time_required
+    3.0
+  end
+end
+
 RSpec.describe Task do
   describe '.new' do
     it 'instantiates' do
