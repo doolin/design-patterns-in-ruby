bcc69915f6173286566fe86f06f5608a1d4db708
David Doolin <david.doolin@gmail.com>
2017-10-21 07:44:49 -0700

    p. 116 sum the times from the task tree

--- ch6/ch6.rb
+++ ch6/ch6.rb
@@ -34,6 +34,36 @@ class MixTask < Task
   end
 end
 
+class MakeBatterTask < Task
+  def initialize
+    super('make batter')
+    @sub_tasks = []
+    add_sub_task(AddDryIngredientsTask.new)
+    # add_sub_task(AddLiquidsTask.new)
+    add_sub_task(MixTask.new)
+  end
+
+  def add_sub_task(task)
+    @sub_tasks << task
+  end
+
+  def delete_sub_task(task)
+    @sub_tasks.delete(task)
+  end
+
+  def get_time_required
+    @sub_tasks.sum(&:get_time_required)
+  end
+end
+
+RSpec.describe MakeBatterTask do
+  describe 'get_time_required' do
+    it 'sums batter making tasks' do
+      expect(MakeBatterTask.new.get_time_required).to eq 4.0
+    end
+  end
+end
+
 RSpec.describe Task do
   describe '.new' do
     it 'instantiates' do
