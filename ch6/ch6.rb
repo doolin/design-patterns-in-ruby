7a6b81cf4fe16a080bddead14f5f8cae3f33a183
David Doolin <david.doolin@gmail.com>
2017-10-22 07:13:08 -0700

    p. 117 composite task
    This is pretty cool, but I can already see that
    I would do it differently by flattening the Task
    inheritance structure.

--- ch6/ch6.rb
+++ ch6/ch6.rb
@@ -56,6 +56,25 @@ class MakeBatterTask < Task
   end
 end
 
+class CompositeTask < Task
+  def initiliaze(name)
+    super(name)
+    @sub_tasks = []
+  end
+
+  def add_sub_task(task)
+    @sub_tasks << task
+  end
+
+  def remove_sub_task(task)
+    @sub_tasks.delete(task)
+  end
+
+  def get_time_required
+    @sub_tasks.sum(&:get_time_required)
+  end
+end
+
 RSpec.describe MakeBatterTask do
   describe 'get_time_required' do
     it 'sums batter making tasks' do
