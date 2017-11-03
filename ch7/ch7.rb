4c4aa8fbb7e3baca98eeae896783ec1ae6a216b3
David Doolin <david.doolin@gmail.com>
2017-11-03 04:15:50 -0700

    p. 134 account and portfolio example

--- ch7/ch7.rb
+++ ch7/ch7.rb
@@ -113,3 +113,35 @@ RSpec.describe Enumerable do
     end
   end
 end
+
+class Account
+  attr_accessor :name, :balance
+
+  def initialize(name, balance)
+    @name = name
+    @balance = balance
+  end
+
+  def <=>(other)
+    balance <=> other.balance
+  end
+end
+
+class Portfolio
+  include Enumerable
+
+  def initialize
+    @accounts = []
+  end
+
+  def each(&block)
+    @accounts.each(&block)
+  end
+
+  def add_account(account)
+    @accounts << account
+  end
+end
+
+RSpec.describe Account do
+end
