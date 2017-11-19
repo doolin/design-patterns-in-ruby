2fe42809aa81f48b19feb2afb92a42772c9a5a39
David Doolin <david.doolin@gmail.com>
2017-11-19 06:05:40 -0800

    p. 178 protection proxy

--- ch10/ch10.rb
+++ ch10/ch10.rb
@@ -2,6 +2,8 @@
 
 require 'rspec/autorun'
 
+# Chapter 10, the proxy pattern.
+
 class BankAccount
   attr_reader :balance
 
@@ -19,9 +21,133 @@ class BankAccount
 end
 
 RSpec.describe BankAccount do
+  subject(:account) { BankAccount.new(100) }
+
   describe '#deposit' do
+    it '10 dollars' do
+      account.deposit(10)
+      expect(account.balance).to eq 110
+    end
   end
 
   describe '#withdraw' do
+    example '10 dollars' do
+      account.withdraw(10)
+      expect(account.balance).to eq 90
+    end
   end
 end
+
+class BankAccountProxy
+  def initialize(real_object)
+    @real_object = real_object
+  end
+
+  def balance
+    @real_object.balance
+  end
+
+  def deposit(amount)
+    @real_object.deposit(amount)
+  end
+
+  def withdraw(amount)
+    @real_object.withdraw(amount)
+  end
+end
+
+RSpec.describe BankAccountProxy do
+  let(:real_account) { BankAccount.new(100) }
+
+  subject(:proxy) { BankAccountProxy.new(real_account) }
+
+  describe '#deposit' do
+    it '10 dollars' do
+      proxy.deposit(10)
+      expect(proxy.balance).to eq 110
+    end
+  end
+
+  describe '#withdraw' do
+    example '10 dollars' do
+      proxy.withdraw(10)
+      expect(proxy.balance).to eq 90
+    end
+  end
+end
+
+require 'etc'
+class AccountProtectionProxy
+  def initialize(real_account, owner_name)
+    @real_account = real_account
+    @owner_name = owner_name
+  end
+
+  def balance
+    check_access
+    @real_account.balance
+  end
+
+  def deposit(amount)
+    check_access
+    @real_account.deposit(amount)
+  end
+
+  def withdraw(amount)
+    check_access
+    @real_account.withdraw(amount)
+  end
+
+  def check_access
+    if Etc.getlogin != @owner_name
+      raise "Illegal access: #{Etc.getlogin} cannot access account"
+    end
+  end
+end
+
+RSpec.describe AccountProtectionProxy do
+  let(:real_account) { BankAccount.new(100) }
+
+  context 'with correct owner' do
+    subject(:proxy) { AccountProtectionProxy.new(real_account, 'doolin') }
+
+    describe '#deposit' do
+      it '10 dollars' do
+        proxy.deposit(10)
+        expect(proxy.balance).to eq 110
+      end
+    end
+
+    describe '#withdraw' do
+      example '10 dollars' do
+        proxy.withdraw(10)
+        expect(proxy.balance).to eq 90
+      end
+    end
+  end
+
+  context 'with wrong owner' do
+    let!(:wrong_owner) { 'doolin' }
+    subject(:proxy) { AccountProtectionProxy.new(real_account, 'foobar') }
+
+    describe '#deposit' do
+      it '10 dollars' do
+        expect {
+          proxy.deposit(10)
+        }.to raise_error(RuntimeError, /#{wrong_owner}/)
+        expect(real_account.balance).to eq 100
+      end
+    end
+
+    describe '#withdraw' do
+      example '10 dollars' do
+        expect {
+          proxy.withdraw(10)
+        }.to raise_error(RuntimeError, /#{wrong_owner}/)
+        expect(real_account.balance).to eq 100
+      end
+    end
+  end
+end
+
+
