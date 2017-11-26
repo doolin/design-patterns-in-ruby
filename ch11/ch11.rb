09279802fd793fbe743f398c082ed517a7917746
David Doolin <david.doolin@gmail.com>
2017-11-26 05:14:16 -0800

    p. 198 decorator classes

--- ch11/ch11.rb
+++ ch11/ch11.rb
@@ -4,6 +4,7 @@ require 'rspec/autorun'
 
 # Decorators
 
+# An example class, won't be using this one.
 class SimpleWriter
   def initialize(path)
     @file = File.open(path, 'w')
@@ -27,5 +28,39 @@ class SimpleWriter
   end
 end
 
-RSpec.describe SimpleWriter do
+class WriterDecorator
+  def initialize(real_writer)
+    @real_writer = real_writer
+  end
+
+  def write_line(line)
+    @real_writer.write_line(line)
+  end
+
+  def pos
+    @real_writer.pos
+  end
+
+  def rewind
+    @real_writer.rewind
+  end
+
+  def close
+    @real_writer.close
+  end
+end
+
+class NumberingWriter < WriterDecorator
+  def initialize(real_writer)
+    super(real_writer)
+    @line_number = 1
+  end
+
+  def write_line(line)
+    @real_writer.write_line("#{line_number}: #{line}")
+    @line_number += 1
+  end
+end
+
+RSpec.describe NumberingWriter do
 end
