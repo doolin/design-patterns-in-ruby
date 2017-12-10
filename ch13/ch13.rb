f0065853e2b1d73f8bad43ddc0bd9b300acf01cf
David Doolin <david.doolin@gmail.com>
2017-12-10 06:26:10 -0800

    p. 228 start of factory chapter

--- ch13/ch13.rb
+++ ch13/ch13.rb
@@ -2,3 +2,22 @@
 
 require 'rspec/autorun'
 
+class Duck
+  def initialize(name)
+    @name = name
+  end
+
+  def eat
+    "Duck #{name} is eating"
+  end
+
+  def speak
+    "Duck #{name} is speaking"
+  end
+
+  def sleep
+    "Duck #{name} sleeps quietly"
+  end
+end
+
+
