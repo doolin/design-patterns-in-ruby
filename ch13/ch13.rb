642bdbe55f2f5eabd4466b4eaf1327db9cd2285b
David Doolin <david.doolin@gmail.com>
2017-12-21 04:11:50 -0800

    p. 242 example usage

--- ch13/ch13.rb
+++ ch13/ch13.rb
@@ -299,3 +299,13 @@ class OrganismFactory
     @plant_class.new(name)
   end
 end
+
+jungle_organism_factory = OrganismFactory.new(Tree, Tiger)
+pond_organism_factory = OrganismFactory.new(WaterLily, Frog)
+
+jungle = Habitat.new(1, 4, jungle_organism_factory)
+jungle.simulate_one_day
+
+pond = Habitat.new(2, 4, pond_organism_factory)
+pond.simulate_one_day
+
