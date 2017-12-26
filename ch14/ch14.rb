edf3e782a50618fcda7b93a3996d3d5241e6a0c1
David Doolin <david.doolin@gmail.com>
2017-12-26 05:12:36 -0800

    p. 258 method_missing for poor mans interpreter
    Not a fan of method missing, even though it's pretty
    cool as used here. Sort of cool maybe. It would be
    cooler if it didn't implement a switchyard to handle
    the various cases.

--- ch14/ch14.rb
+++ ch14/ch14.rb
@@ -68,6 +68,19 @@ class ComputerBuilder
   def memory_size(size_in_mb)
     @computer.motherboard.memory_size = size_in_mb
   end
+
+  # p. 258 builders with magic methods
+  def method_missing(name, *args)
+    words = name.to_s.split('_')
+    return super(name, *args) unless words.shift == 'add'
+    words.each do |word|
+      next if word == 'and'
+      add_cd if word == 'cd'
+      add_dvd if word == 'dvd'
+      add_hard_disk(100_000) if word == 'harddisk'
+      turbo if word == 'turbo'
+    end
+  end
 end
 
 # p. 253
@@ -135,3 +148,15 @@ class Computer
     raise 'No hard disk' unless hard_disk
   end
 end
+
+RSpec.describe ComputerBuilder do
+  describe '#method_missing' do
+    it 'builds' do
+      builder = LaptopBuilder.new
+      builder.add_dvd_and_harddisk
+      computer = builder.computer
+      expect(computer.drives.count).to eq 2
+    end
+  end
+end
+
