7fb6ed010868bc9e57c33943ebfa1115eed25789
David Doolin <david.doolin@gmail.com>
2017-12-29 06:44:44 -0800

    p. 287, started packrat dsl
    This chapter makes a jump in complexity by using the
    file finding code written in the last chapter. This means
    I need to rewrite all that file finding code into proper
    Ruby structure; it won't work left in a giant rspec
    autorun file. This will put me behind my anticipated
    finish time for the book, but that's ok, as the material
    is starting to get interesting.

---
+++ ch16/backup.rb
@@ -0,0 +1,25 @@
+class Backup
+  include Singleton
+
+  attr_accessor :backup_directory, :interval
+  attr_reader :data_sources
+
+  def initialize
+    @data_source = []
+    @backup_directory = '/backup'
+    @interval = 60
+  end
+
+  def backup_files
+    this_backup_dir = Time.new.ctime.tr(' :', '_')
+    this_backup_path = File.join(backup_directory, this_backup_dir)
+    @data_sources.each { |source| source.backup(this_backup_path) }
+  end
+
+  def run
+    while true
+      backup_files
+      sleep(@interval * 60)
+    end
+  end
+end
---
+++ ch16/ch16.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+require 'rspec/autorun'
+require 'pry'
+
+require 'finder'
