c92ba9d074f622d7466eab84047cbf63ebe5ff8b
David Doolin <david.doolin@gmail.com>
2018-01-20 05:24:21 -0800

    p. 301 nocturnal, diurnal

--- ch17/ch17.rb
+++ ch17/ch17.rb
@@ -56,3 +56,25 @@ module Herbivore
     'flat'
   end
 end
+
+module Nocturnal
+  def sleep_time
+    'day'
+  end
+
+  def awake_time
+    'night'
+  end
+end
+
+module Diurnal
+  def sleep_time
+    'night'
+  end
+
+  def awake_time
+    'day'
+  end
+end
+
+
