083810a2a56181df1b5a68a2d001e36da57d053b
David Doolin <david.doolin@gmail.com>
2018-02-03 06:43:38 -0800

    p. 318 crawling along, super busy at work
    Totally slammed at work, leaving very early and
    getting home very late, just to tired to put any
    real effort into this project at the moment.

--- ch18/ch18.rb
+++ ch18/ch18.rb
@@ -25,5 +25,11 @@ class SmtpAdapter
   def send(message)
     from_address = message.from.user + '@' + message.from.host
     to_address = message.to.user + '@' + message.to.host
+
+    email_text = "From: #{from_address}\n"
+    email_text += "To: #{to_address}\n"
+    email_text += "Subject: Forwarded message\n"
+    email_text += "\n"
+    email_text += message.text
   end
 end
