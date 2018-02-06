19222b4ed0fb2f0ba17805fd75f2ede5b1f50b7c
David Doolin <david.doolin@gmail.com>
2018-02-06 04:31:54 -0800

    p. 319 file adapter...

--- ch18/ch18.rb
+++ ch18/ch18.rb
@@ -51,6 +51,21 @@ class HttpAdapter
   def send(message)
     Net::HTTP.start(message.to.host, message.to.port) do |http|
       http.post(message.to.path, message.text)
-    en
+    end
+  end
+end
+
+class FileAdapter
+  def send(message)
+    #
+    # Get the path from the URL
+    # and remove the leading '/'
+    #
+    to_path = message.to.path
+    to_path.slice!(0)
+
+    File.open(to_path, 'w') do |f|
+      f.write(message.text)
+    end
   end
 end
