e292be93ac7d5a8e08220ab0953cc2621aae3485
David Doolin <david.doolin@gmail.com>
2018-02-08 04:26:08 -0800

    filled out message gateway

--- ch18/ch18.rb
+++ ch18/ch18.rb
@@ -79,4 +79,20 @@ class MessageGateway
     adapter = adapter_for(message)
     adapter.send_message(message)
   end
+
+  def adapter_for(message)
+    protocol = message.to.scheme
+    adapter_class = protocol.capitalize + 'Adapter'
+    adapter_class = self.class.const_get(adapter_class)
+    adapter_class.new
+  end
+
+  def load_adapters
+    lib_dir = File.dirname(__FILE__)
+    full_pattern = File.join(lib_dir, 'adapter', '*.rb')
+    Dir.glob(full_pattern).each { |file| require file }
+  end
+end
+
+RSpec.describe MessageGateway do
 end
