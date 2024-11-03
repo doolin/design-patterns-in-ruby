eec4ac4a741ec48ce90cf9cc73c3fff06e308a67
dave doolin <david.doolin@gmail.com>
2024-11-03 04:22:21 -0800

    Linted foxy
    Foxy doesn't run at the moment as I don't have
    quartx running for X11. But it's cleaned up!

--- foxy.rb
+++ foxy.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 # This is an X11 application, will run on OSX with XQuartz.
 
@@ -6,30 +7,30 @@ require 'fox16'
 
 include Fox
 
-application = FXApp.new("CompositeGUI", 'CompositeGUI')
+application = FXApp.new('CompositeGUI', 'CompositeGUI')
 
-main_window = FXMainWindow.new(application, "Composite", nil, nil, DECOR_ALL)
+main_window = FXMainWindow.new(application, 'Composite', nil, nil, DECOR_ALL)
 main_window.width = 600
 main_window.height = 400
 
 super_frame = FXVerticalFrame.new(main_window,
                                   LAYOUT_FILL_X | LAYOUT_FILL_Y)
-FXLabel.new(super_frame, "Text Editor Application")
+FXLabel.new(super_frame, 'Text Editor Application')
 
 text_editor = FXHorizontalFrame.new(super_frame,
-                                  LAYOUT_FILL_X | LAYOUT_FILL_Y)
+                                    LAYOUT_FILL_X | LAYOUT_FILL_Y)
 
 text = FXText.new(text_editor, nil, 0,
-                  TEXT_READONLY|TEXT_WORDWRAP|LAYOUT_FILL_X|LAYOUT_FILL_Y)
+                  TEXT_READONLY | TEXT_WORDWRAP | LAYOUT_FILL_X | LAYOUT_FILL_Y)
 
 text.text = 'This is some text.'
 
 button_frame = FXVerticalFrame.new(text_editor,
                                    LAYOUT_SIDE_RIGHT | LAYOUT_FILL_Y)
 
-FXButton.new(button_frame, "Cut")
-FXButton.new(button_frame, "Copy")
-FXButton.new(button_frame, "Paste")
+FXButton.new(button_frame, 'Cut')
+FXButton.new(button_frame, 'Copy')
+FXButton.new(button_frame, 'Paste')
 
 application.create
 main_window.show(PLACEMENT_SCREEN)
